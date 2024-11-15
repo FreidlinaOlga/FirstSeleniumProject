5e5a1fdc9c53c422eb6f70b58433becdd8e86f93
FreidlinaOlga <olgazagorodneva45@gmail.com>
2024-11-15 13:07:18 +0100

    'Updated'

---
+++ src/test/java/com/ait/qa49/FirstSeleniumTest.java
@@ -0,0 +1,45 @@
+package com.ait.qa49;
+
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+import java.time.Duration;
+
+public class FirstSeleniumTest {
+    WebDriver driver;
+    //before method - setUp (или init)
+    @BeforeMethod //гарантия отработки вначале
+    public void  setUp(){
+      driver = new ChromeDriver();
+     // driver.get("https://www.google.com"); // без истории
+        // maximize browser to window
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.google.com"); //с историей
+        // wait for all elements on the site to load before starting test
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://ilcarro.web.app");
+         driver.navigate().back();
+         driver.navigate().forward();
+         driver.navigate().refresh(); // обновлять
+
+    }
+    //test - название четкое и прозрачное
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Google opened!");
+    }
+
+    //after -tearDown
+
+@AfterMethod(enabled = true)
+    public void tearDown(){
+       // driver.quit(); закрывает все вкладки
+    driver.close(); // закрывает текущую вкладку, если открыта одна вкладка, то закроется и браузер
+}
+
+}
